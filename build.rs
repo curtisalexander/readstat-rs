15a04c60b8403923b1b0d3e916f5afabf7a1053e
Curtis Alexander <calex@calex.org>
2020-10-29 18:31:14 -0500

    initial commit; setting up machinery

---
+++ build.rs
@@ -0,0 +1,42 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to tell rustc to link the system bzip2
+    // shared library.
+    println!("cargo:rustc-link-lib=bz2");
+
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("wrapper.h")
+        // Select which functions and types to build bindins for
+        .whitelist_function("readstat_get_row_count")
+        .whitelist_function("readstat_parse_sas7bdat")
+        .whitelist_function("readstat_parser_init")
+        .whitelist_function("readstat_set_metadata_handler")
+        .whitelist_type("readstat_error_t")
+        .whitelist_type("readstat_metadata_t")
+        .whitelist_type("readstat_parser_t")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ wrapper.h
@@ -0,0 +1,1 @@
+#include "../ReadStat/src/readstat.h"
