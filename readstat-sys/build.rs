835070e4f2740ce98967d35b03dd58067a1ce47e
Curtis Alexander <calex@calex.org>
2020-11-03 21:18:03 -0600

    Add bindgen functionality to sys crate

--- readstat-sys/build.rs
+++ readstat-sys/build.rs
@@ -1,3 +1,5 @@
+extern crate bindgen;
+
 use cc;
 use dunce;
 use std::env;
@@ -34,10 +36,10 @@ fn main() {
     let project_dir = dunce::canonicalize(PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())).unwrap();
 
     let src = project_dir.join("vendor").join("ReadStat").join("src");
-    let sas = project_dir.join("vendor").join("ReadStat").join("src").join("sas");
-    let spss = project_dir.join("vendor").join("ReadStat").join("src").join("spss");
-    let stata = project_dir.join("vendor").join("ReadStat").join("src").join("stata");
-    let txt = project_dir.join("vendor").join("ReadStat").join("src").join("txt");
+    let sas = src.join("sas");
+    let spss = src.join("spss");
+    let stata = src.join("stata");
+    let txt = src.join("txt");
     
     let src_c_files = get_all_c_files(&src);
     let sas_c_files = get_all_c_files(&sas);
@@ -54,4 +56,45 @@ fn main() {
         .include(&src)
         .warnings(false)
         .compile("readstat");
+
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    // Linking
+    println!("cargo:rustc-link-lib=static=readstat");
+    // println!("cargo:rustc-link-search=/home/calex/code/readstat-rs/readstat/target/debug/build/readstat-sys-87fcd7a4da21a534/out");
+    // println!("cargo:rustc-link-lib=readstat");
+    // println!("cargo:rustc-link-search=/usr/local/lib");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("wrapper.h")
+        // Select which functions and types to build bindings for
+        .whitelist_function("readstat_get_row_count")
+        .whitelist_function("readstat_parse_sas7bdat")
+        .whitelist_function("readstat_parser_free")
+        .whitelist_function("readstat_parser_init")
+        .whitelist_function("readstat_set_metadata_handler")
+        .whitelist_type("readstat_error_t")
+        .whitelist_type("readstat_metadata_t")
+        .whitelist_type("readstat_parser_t")
+        .whitelist_type("READSTAT_HANDLER_OK")
+        .whitelist_type("READSTAT_OK")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
--- readstat-sys/src/lib.rs
+++ readstat-sys/src/lib.rs
@@ -1,7 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![allow(dead_code)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
