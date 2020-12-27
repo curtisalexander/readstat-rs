7ec7e1c60922a9b00633cda7158c59097b58cb08
Curtis Alexander <calex@calex.org>
2020-12-26 22:35:35 -0600

    Try to explicitly set linker to point to macos libs

--- readstat-sys/build.rs
+++ readstat-sys/build.rs
@@ -86,7 +86,8 @@ fn main() {
     }
 
     if target.contains("apple-darwin") {
-        println!("cargo:rustc-link-search=/usr/local/opt/libiconv/lib");
+        println!("cargo:rustc-link-search=/usr/lib/libiconv");
+        // println!("cargo:rustc-link-search=/usr/local/opt/libiconv/lib");
     }
 
     // Compile
