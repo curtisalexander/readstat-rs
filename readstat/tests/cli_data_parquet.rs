9ceadcc4d5ef85a3ebcbba7d72f229bf12fb31f9
Curtis Alexander <calex@calex.org>
2022-08-20 22:09:54 -0500

    Simplify

--- readstat/tests/cli_data_parquet.rs
+++ readstat/tests/cli_data_parquet.rs
@@ -1,30 +1,24 @@
 use ::predicates::prelude::*; // Used for writing assertions
 use assert_cmd::prelude::*; // Add methods on commands
-use assert_fs::TempDir;
+use assert_fs::NamedTempFile;
 use polars::prelude::*;
-use std::{
-    fs::File,
-    ops::Deref,
-    path::{Path, PathBuf},
-    process::Command,
-    result::Result,
-};
+use std::{fs::File, path::PathBuf, process::Command, result::Result};
 
 fn cli_data_to_parquet(
     base_file_name: &str,
-) -> Result<(Command, OutFile), Box<dyn std::error::Error>> {
-    let out_file = OutFile::new(base_file_name);
+) -> Result<(Command, NamedTempFile), Box<dyn std::error::Error>> {
+    let tempfile = NamedTempFile::new(format!("{}.parquet", base_file_name))?;
 
     let mut cmd = Command::cargo_bin("readstat")?;
 
     cmd.arg("data")
         .arg(format!("tests/data/{}.sas7bdat", base_file_name))
         .args(["--format", "parquet"])
-        .args(["--output", out_file.out_path.as_os_str().to_str().unwrap()])
-        // .args(["--stream-rows", "500"])
+        .args(["--output", tempfile.as_os_str().to_str().unwrap()])
+        .args(["--stream-rows", "500"])
         .arg("--overwrite");
 
-    Ok((cmd, out_file))
+    Ok((cmd, tempfile))
 }
 
 fn parquet_to_df(path: PathBuf) -> Result<DataFrame, Box<dyn std::error::Error>> {
@@ -35,42 +29,20 @@ fn parquet_to_df(path: PathBuf) -> Result<DataFrame, Box<dyn std::error::Error>>
     Ok(df)
 }
 
-pub struct OutFile {
-    pub out_path: PathBuf,
-    pub _tempdir: TempDir,
-}
-
-impl OutFile {
-    pub fn new(filename: &str) -> Self {
-        let _tempdir = TempDir::new().unwrap();
-        let mut out_path = PathBuf::from(_tempdir.path());
-        out_path.push(format!("{}.parquet", filename));
-
-        Self { out_path, _tempdir }
-    }
-}
-
-impl Deref for OutFile {
-    type Target = Path;
-
-    fn deref(&self) -> &Self::Target {
-        self.out_path.deref()
-    }
-}
 #[test]
 fn cars_to_parquet() {
-    let (mut cmd, out_file) = cli_data_to_parquet("cars").unwrap();
+    let (mut cmd, tempfile) = cli_data_to_parquet("cars").unwrap();
 
     cmd.assert().success().stdout(predicate::str::contains(
         "In total, wrote 1,081 rows from file cars.sas7bdat into cars.parquet",
     ));
 
-    let df = parquet_to_df(out_file.out_path).unwrap();
+    let df = parquet_to_df(tempfile.to_path_buf()).unwrap();
 
     let (height, width) = df.shape();
 
     assert_eq!(height, 1081);
     assert_eq!(width, 13);
 
-    out_file._tempdir.close().unwrap();
+    tempfile.close().unwrap();
 }
