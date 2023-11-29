8ce9c9d50f3b6dfe8eb5afa066d7f2bf31bf4d61
Curtis Alexander <6118829+curtisalexander@users.noreply.github.com>
2023-11-29 09:23:40 -0600

    Addresses issue #78.  When converting a C string into a Rust String do not assume that the byte sequence is valid UTF-8.  Instead, utilize a lossy conversion - making functionality more robust.

--- crates/readstat/src/common.rs
+++ crates/readstat/src/common.rs
@@ -37,6 +37,12 @@ pub unsafe fn ptr_to_string(x: *const i8) -> String {
     if x.is_null() {
         String::new()
     } else {
-        CStr::from_ptr(x).to_str().unwrap().to_owned()
+        // From Rust documentation - https://doc.rust-lang.org/std/ffi/struct.CStr.html
+        let cstr = CStr::from_ptr(x);
+        // Get copy-on-write Cow<'_, str>, then guarantee a freshly-owned String allocation
+        String::from_utf8_lossy(cstr.to_bytes()).to_string()
+
+        // Prior art
+        // CStr::from_ptr(x).to_str().unwrap().to_owned()
     }
 }
