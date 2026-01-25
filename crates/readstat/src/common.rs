1404dd57057a499724a4776e034ae94072eef38d
Curtis Alexander <6118829+curtisalexander@users.noreply.github.com>
2026-01-25 10:49:56 -0600

    Rust edition 2024

--- crates/readstat/src/common.rs
+++ crates/readstat/src/common.rs
@@ -33,12 +33,12 @@ pub fn build_offsets(
 }
 
 // String out from C pointer
-pub unsafe fn ptr_to_string(x: *const i8) -> String {
+pub fn ptr_to_string(x: *const i8) -> String {
     if x.is_null() {
         String::new()
     } else {
         // From Rust documentation - https://doc.rust-lang.org/std/ffi/struct.CStr.html
-        let cstr = CStr::from_ptr(x);
+        let cstr = unsafe { CStr::from_ptr(x) };
         // Get copy-on-write Cow<'_, str>, then guarantee a freshly-owned String allocation
         String::from_utf8_lossy(cstr.to_bytes()).to_string()
     }
